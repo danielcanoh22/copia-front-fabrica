4856657b58f071a6d1352c23d107f054e2dc32ce
danielcanoh22 <daniel.canoh@udea.edu.co>
2024-11-22 20:03:29 -0500

    add id

--- src/pages/checkinA/index.tsx
+++ src/pages/checkinA/index.tsx
@@ -5,6 +5,7 @@ const CheckinA = () => {
     <div className="flex flex-col gap-4 justify-center items-center p-4">
       <h1 className="text-2xl font-bold">Checkin A</h1>
       <Link
+        id="checkin-confirmation-btn"
         href="/checkinA/validation"
         className="bg-primary text-white p-2 rounded-md"
         target="_blank"
@@ -12,6 +13,7 @@ const CheckinA = () => {
         Ir a la ventana de confirmación
       </Link>
       <Link
+        id="checkin-form-btn"
         href="/checkinA/confirmation"
         className="bg-primary text-white p-2 rounded-md"
         target="_blank"
